b9231c4eebc3fb94d4aecab734477f8e9db68e9e
Christian <chrnielsen2003@gmail.com>
2020-12-16 15:59:34 +0100

    b

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello world"); 
+var randomTal = Math.floor(Math.random() * 4);
+
+fetch(`http://localhost:3000/api/v1/cheeses?offset=${randomTal}`)
+.then(response => response.json())
+.then(function (data){
+
+    
+    var ost = data.results 
+    var osteListe = document.querySelector(".osteListe");
+    
+    ost.forEach(oste => {
+        const osteList = document.createElement(`div`)
+        osteList.classList.add(`ostCard`)
+        osteList.innerHTML = `
+        <img src="https://via.placeholder.com/150x200">
+        <h3 class="ostNavn">${oste.name}</h3>
+        <p>${oste.strength}</p>
+        <p class="ostPris">${oste.price.$numberDecimal} kr</p>
+        <button class="addToCart">Læg i kurv</button>
+        `
+        osteListe.appendChild(osteList)
+    });
+});
