591608e0e5c7a8203f8f92eaff5ffe2840fe4df0
Christian <chrnielsen2003@gmail.com>
2020-12-17 13:11:55 +0100

    count

--- app.js
+++ app.js
@@ -21,3 +21,84 @@ fetch(`http://localhost:3000/api/v1/cheeses?offset=${randomTal}`)
         osteListe.appendChild(osteList)
     });
 });
+
+
+let form = document.querySelector(".contact__form")
+let inputFields = document.querySelectorAll(".input__form");
+let success = true;
+var emailInput = document.querySelector("#email");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    inputFields.forEach(function (input) {
+        form.querySelector(".statusMessage." + input.name).innerText = "";
+    });
+
+    inputFields.forEach(function (input) {
+        if (input.type == "number" && !/^[0-9]+$/.test(input.value) && input.value != "") {
+            form.querySelector(".statusMessage." + input.name).innerText = "der må kun være tal.";
+            success = false;
+
+        }
+
+        if (input.hasAttribute("required") && input.value == "") {
+            form.querySelector(".statusMessage." + input.name).innerText = "Udfyld dette felt.";
+            success = false;
+
+        }
+
+        var at = form.email.value.indexOf("@");
+        var dot = form.email.value.lastIndexOf(".");
+        var emailStatus = document.querySelector(".statusMessageEmail")
+
+        if (at <= 0) {
+            form.email.focus();
+            emailStatus.innerText = "Skriv gyldig email.";
+            success = false;
+        }
+        if (dot < at + 2) {
+            form.email.focus();
+            emailStatus.innerText = "Skriv gyldig email.";
+            success = false;
+        }
+        if (form.email.value.length <= dot + 2) {
+            form.email.focus();
+            emailStatus.innerText = "Skriv gyldig email.";
+            success = false;
+        }
+
+
+    });
+    if (!success) return
+});
+
+
+function cart(){
+    var count = localStorage.getItem("cart");
+    var cartNum = document.querySelector(".cartNumber");
+    var addToCart = document.querySelector(".addToCart");
+    
+    cartNum.innerHTML = localStorage.getItem("cart")
+
+    if (localStorage.getItem("")) {
+        count = 0;
+    }
+
+    window.addEventListener("click", function(){
+        count++;
+        cartNum.innerHTML=count;
+        
+        localStorage.setItem("cart", count);
+
+    })
+    
+    /* 
+    
+    addToCart.addEventListener("click", function(event){
+        /* localStorage.setItem("cart" +1) 
+        console.log(addToCart);
+    }) 
+    */
+}
+
+cart()
